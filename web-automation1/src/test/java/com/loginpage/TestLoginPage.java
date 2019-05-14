78ff0ccc901fccfb0202b22251a0132ea722c607
04saikiran <nawate.k@gmail.com>
2019-05-14 18:51:30 +0530

    first commit

--- web-automation1/src/test/java/com/loginpage/TestLoginPage.java
+++ web-automation1/src/test/java/com/loginpage/TestLoginPage.java
@@ -22,6 +22,7 @@ public class TestLoginPage {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
 		url = "http://demo.automationtesting.in/";
+		// Test commit //
 	}
 
 	@Test
